3777480febb8005ff9e3b8f3f14b4c4ce88f71cf
Joshua Borkowski-Clark <1917489+JoBoCl@users.noreply.github.com>
2023-12-09 12:08:45 +0100

    Revert "Allow the solver to be mutable"
    This reverts commit 54224c5ad48bbdefb700621775a60af5481d4ef4.
    
    This makes things more fiddly than it needs to be.  If mutable state is
    required, prefer to clone within each part.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub trait Solver {
-  fn part_one(&mut self) -> std::io::Result<String>;
-  fn part_two(&mut self) -> std::io::Result<String>;
+  fn part_one(&self) -> std::io::Result<String>;
+  fn part_two(&self) -> std::io::Result<String>;
 }
