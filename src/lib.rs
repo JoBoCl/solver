94c6722b89f36ccc143da13a7d094e97526d3343
Joshua Borkowski-Clark <1917489+JoBoCl@users.noreply.github.com>
2023-12-11 21:26:10 +0100

    Make solvers castable to Any to improve testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-pub trait Solver {
+use std::any::Any;
+
+pub trait Solver: SolverToAny {
   fn part_one(&self) -> std::io::Result<String>;
   fn part_two(&self) -> std::io::Result<String>;
+}
+
+pub trait SolverToAny: 'static {
+    fn as_any(&self) -> &dyn Any;
 }
