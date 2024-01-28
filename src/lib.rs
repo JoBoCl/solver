182788ce184dd57b036bd342e47af606b62c5860
Joshua Borkowski-Clark <1917489+JoBoCl@users.noreply.github.com>
2024-01-28 12:41:33 +0100

    Replace std::io::Result with anyhow::Result

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
+use anyhow::Result;
 use std::any::Any;
 
 pub trait Solver: SolverToAny {
-  fn part_one(&self) -> std::io::Result<String>;
-  fn part_two(&self) -> std::io::Result<String>;
+  fn part_one(&self) -> Result<String>;
+  fn part_two(&self) -> Result<String>;
 }
 
 pub trait SolverToAny: 'static {
-    fn as_any(&self) -> &dyn Any;
+  fn as_any(&self) -> &dyn Any;
+}
+
+macro_rules! solver_to_any {
+  ($name:ident) => {
+    impl SolverToAny for $name {
+      fn as_any(&self) -> &dyn std::any::Any { self }
+    }
+  };
 }
